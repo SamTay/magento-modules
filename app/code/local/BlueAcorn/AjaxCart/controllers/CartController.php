dc34b74f20085252cde27be686f28c48aaf388ab
Jim Simon <jim@blueacorn.com>
2014-05-26 15:03:31 -0400

    Initial commit of Ajax Minicart refactor for 1.14

--- app/code/local/BlueAcorn/AjaxCart/controllers/CartController.php
+++ app/code/local/BlueAcorn/AjaxCart/controllers/CartController.php
@@ -1,149 +1,103 @@
 <?php
-/**
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- *
- * @copyright  Copyright (c) 2013 Blue Acorn (http://www.blueacorn.com)
- * @author: Thomas Slade
- * @namespace: BlueAcorn
- * @module: AjaxCart
- *
- * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
-
-require_once Mage::getModuleDir('controllers', 'Mage_Checkout') . DS . 'CartController.php';
-
+require_once "Mage/Checkout/controllers/CartController.php";
 class BlueAcorn_AjaxCart_CartController extends Mage_Checkout_CartController {
-
     protected $_messages = array();
 
     /**
      * Add product to shopping cart action
+     *
+     * @return Mage_Core_Controller_Varien_Action
+     * @throws Exception
      */
     public function addAction()
     {
-        if(Mage::getStoreConfig('checkout/cart/redirect_to_cart')){
-            parent::addAction();
-        } else {
-            $cart   = $this->_getCart();
-            $params = $this->getRequest()->getParams();
-            try {
-                if (isset($params['qty'])) {
-                    $filter = new Zend_Filter_LocalizedToNormalized(
-                        array('locale' => Mage::app()->getLocale()->getLocaleCode())
-                    );
-                    $params['qty'] = $filter->filter($params['qty']);
-                }
-
-                $product = $this->_initProduct();
-                $related = $this->getRequest()->getParam('related_product');
-
-                /**
-                 * Check product availability
-                 */
-                if (!$product) {
-                    return;
-                }
-
-                $cart->addProduct($product, $params);
-                if (!empty($related)) {
-                    $cart->addProductsByIds(explode(',', $related));
-                }
-
-                $cart->save();
-                $this->_getSession()->setCartWasUpdated(true);
-
-                $this->addMessage('cart_qty', $this->_getQuote()->getItemsQty());
-
-                /**
-                 * @todo remove wishlist observer processAddToCart
-                 */
-                Mage::dispatchEvent('checkout_cart_add_product_complete',
-                    array('product' => $product, 'request' => $this->getRequest(), 'response' => $this->getResponse())
+        if (!Mage::getStoreConfig('blueacorn_ajaxcart/general/enabled')) parent::addAction();
+        
+        if (!$this->_validateFormKey()) {
+            $this->_goBack();
+            return;
+        }
+        $cart   = $this->_getCart();
+        $params = $this->getRequest()->getParams();
+        try {
+            if (isset($params['qty'])) {
+                $filter = new Zend_Filter_LocalizedToNormalized(
+                    array('locale' => Mage::app()->getLocale()->getLocaleCode())
                 );
-
-                $this->addMessage('minicart_html', $this->getCartHtml());
-
-                if (!$this->_getSession()->getNoCartRedirect(true)) {
-                    if (!$cart->getQuote()->getHasError()){
-                        $message = $this->__('%s was added to your shopping cart.', Mage::helper('core')->escapeHtml($product->getName()));
-                        $this->addSuccess($message);
-                    }
-                }
-            } catch (Mage_Core_Exception $e) {
-                if ($this->_getSession()->getUseNotice(true)) {
-                    $this->addError(Mage::helper('core')->escapeHtml($e->getMessage()));
-                    $this->_getSession()->addNotice(Mage::helper('core')->escapeHtml($e->getMessage()));
-                } else {
-                    $messages = array_unique(explode("\n", $e->getMessage()));
-                    foreach ($messages as $message) {
-                        $this->addError($this->_getSession()->addError(Mage::helper('core')->escapeHtml($message)));
-                    }
-                }
-                $url = $this->_getSession()->getRedirectUrl(true);
-                if($url){
-                    $this->addMessage('redirect', $url);
-                }
-            } catch (Exception $e) {
-                $this->addError($this->__('Cannot add the item to shopping cart.'));
-                Mage::logException($e);
+                $params['qty'] = $filter->filter($params['qty']);
             }
 
-            $response = $this->getResponse()->setHeader('Content-Type', 'text/javascript');
-            if(array_key_exists('error', $this->_messages)){
-                $response->setHttpResponseCode(500);
+            $product = $this->_initProduct();
+            $related = $this->getRequest()->getParam('related_product');
+
+            /**
+             * Check product availability
+             */
+            if (!$product) {
+                $this->_goBack();
+                return;
             }
-            $response->setBody(Zend_Json::encode($this->_messages));
+
+            $cart->addProduct($product, $params);
+            if (!empty($related)) {
+                $cart->addProductsByIds(explode(',', $related));
+            }
+
+            $cart->save();
+
+            $this->_getSession()->setCartWasUpdated(true);
+
+            /**
+             * @todo remove wishlist observer processAddToCart
+             */
+            Mage::dispatchEvent('checkout_cart_add_product_complete',
+                array('product' => $product, 'request' => $this->getRequest(), 'response' => $this->getResponse())
+            );
+
+            $this->addMessage('minicart_html', $this->getMinicartHtml());
+
+            if (!$this->_getSession()->getNoCartRedirect(true)) {
+                if (!$cart->getQuote()->getHasError()) {
+                    $message = $this->__('%s was added to your shopping cart.', Mage::helper('core')->escapeHtml($product->getName()));
+                    $this->addMessage('success',$message);
+                }
+            }
+        } catch (Mage_Core_Exception $e) {
+            if ($this->_getSession()->getUseNotice(true)) {
+                $this->addMessage('error', Mage::helper('core')->escapeHtml($e->getMessage()));
+                $this->_getSession()->addNotice(Mage::helper('core')->escapeHtml($e->getMessage()));
+            } else {
+                $messages = array_unique(explode("\n", $e->getMessage()));
+                foreach ($messages as $message) {
+                    $this->addMessage('error',Mage::helper('core')->escapeHtml($message));
+                }
+            }
+
+            $url = $this->_getSession()->getRedirectUrl(true);
+            if ($url) {
+                $this->getResponse()->setRedirect($url);
+            } else {
+                $this->_redirectReferer(Mage::helper('checkout/cart')->getCartUrl());
+            }
+        } catch (Exception $e) {
+            $this->addMessage('error', $this->__('Cannot add the item to shopping cart.'));
+            Mage::logException($e);
         }
+
+        $response = $this->getResponse()->setHeader('Content-Type', 'text/javascript');
+        if(array_key_exists('error', $this->_messages)){
+            $response->setHttpResponseCode(500);
+        }
+
+        $response->setBody(Zend_Json::encode($this->_messages));
     }
 
-    /**
-     * Delete shoping cart item action
-     */
-    public function deleteAction()
-    {
-        $id = (int) $this->getRequest()->getParam('id');
-        $noRedirect = $this->getRequest()->getParam('no-redirect');
-        if ($id) {
-            try {
-                $this->_getCart()->removeItem($id)
-                    ->save();
-            } catch (Exception $e) {
-                $this->_getSession()->addError($this->__('Cannot remove the item.'));
-                Mage::logException($e);
-            }
-        }
-        if($noRedirect){
-            $this->getResponse()->setHttpResponseCode(200);
-        } else {
-            $this->_redirectReferer(Mage::getUrl('*/*'));
-        }
-    }
-
-    protected function addMessage($msgType = 'error', $msg = ''){
+    protected function addMessage($msgType, $msg) {
         $this->_messages[$msgType] = $msg;
     }
-
-    protected function addError($msg = ''){
-        $this->addMessage('error', $msg);
-    }
-
-    protected function addSuccess($msg = ''){
-        $this->addMessage('success', $msg);
-    }
-
-    protected function getCartHtml(){
-        $html = Mage::app()->getLayout()
-                    ->createBlock('checkout/cart_sidebar')
-                    ->setTemplate('checkout/cart/cartheader.phtml')
-                    ->addItemRender('simple', 'checkout/cart_item_renderer', 'checkout/cart/sidebar/default.phtml')
-                    ->addItemRender('grouped', 'checkout/cart_item_renderer_grouped', 'checkout/cart/sidebar/default.phtml')
-                    ->addItemRender('configurable', 'checkout/cart_item_renderer_configurable', 'checkout/cart/sidebar/default.phtml')
-                    ->toHtml();
-        
+    protected function getMinicartHtml() {
+        $this->loadLayout();
+        $html = $this->getLayout()->getBlock('minicart_head')->toHtml();
         $html = preg_replace('#<script(.*?)>(.*?)</script>#is', '', $html);
         return $html;
     }
---
+++ skin/frontend/blueacorn/default/js/blueacorn/ajaxcart.js
@@ -0,0 +1,165 @@
+$j(document).ready(function() {
+    if (!$j('body').attr('class').match(/.+catalog-product-view.+/)) {
+        $j('.btn-cart').each(function() {
+            // Extract URL from onclick action
+            var url = $j(this).attr('onclick').match(/setLocation\(\'(.+)\'\)/).last();
+
+            // If URL is an add to cart URL Ajaxify it
+            if(url.match(/.+checkout\/cart\/add\/.+/)) {
+                // Detach onclick action
+                $j(this).attr('onclick', '');
+
+                // Attach AjaxCart observer to add to cart buttons
+                $j(this).bind('click', function () {
+                    // Show fancy box with ajax_cart_form
+                    $j.fancybox.open({
+                        content: '<form action="'+url+'" method="POST" id="ajax_cart_form">' +
+                            '<input type="text" name="qty" id="qty" value="1" />' +
+                            '<button class="button btn-cart" type="submit" title="Add to Cart">' +
+                            '<span><span>Add to Cart</span></span>' +
+                            '</button>' +
+                            '</form>'
+                    });
+
+                    // Bind Ajaxcart observer to submit action
+                    $j('#ajax_cart_form').bind('submit', function(e) {
+                        // Close fancy box
+                        $j.fancybox.close();
+
+                        // Prevent form from submitting
+                        e.preventDefault();
+
+                        // Gather data from product_addtocart_form
+                        var data = {};
+                        $j.each($j(this).serializeArray(), function(i, field) {
+                            data[field.name] = field.value;
+                        });
+
+                        // Instantiate Ajaxcart with url from button
+                        ajaxcart = new Ajaxcart(url);
+
+                        // Let the ajax happen
+                        ajaxcart.addToCart(data);
+                    });
+                });
+            }
+        });
+    } else {
+        // Detach onclick action
+        $j('.btn-cart').attr('onclick','');
+
+        // Atach AjaxCart observer to add to cart button
+        $j('.btn-cart').bind('click', function() {
+            // Validate product_addtocart_form before attempting AJAX
+            if (productAddToCartForm.validator.validate()) {
+                // Instantiate Ajaxcart with url from form action
+                ajaxcart = new Ajaxcart($j('#product_addtocart_form').attr('action'));
+
+                // Gather data from product_addtocart_form
+                var data = {};
+                $j.each($j('#product_addtocart_form').serializeArray(), function(i, field) {
+                    data[field.name] = field.value;
+                });
+
+                // Let the ajax happen
+                ajaxcart.addToCart(data);
+            }
+        });
+    }
+
+    $j(document).bind('rebind-minicart', function(event, data) {
+        var skipContents = $j('.skip-content');
+        var skipLinks = $j('.skip-link');
+
+        skipLinks.on('click', function (e) {
+            e.preventDefault();
+
+            var self = $j(this);
+            var target = self.attr('href');
+
+            // Get target element
+            var elem = $j(target);
+
+            // Check if stub is open
+            var isSkipContentOpen = elem.hasClass('skip-active') ? 1 : 0;
+
+            // Hide all stubs
+            skipLinks.removeClass('skip-active');
+            skipContents.removeClass('skip-active');
+
+            // Toggle stubs
+            if (isSkipContentOpen) {
+                self.removeClass('skip-active');
+            } else {
+                self.addClass('skip-active');
+                elem.addClass('skip-active');
+            }
+        });
+
+        $j('#header-cart').on('click', '.skip-link-close', function(e) {
+            var parent = $j(this).parents('.skip-content');
+            var link = parent.siblings('.skip-link');
+
+            parent.removeClass('skip-active');
+            link.removeClass('skip-active');
+
+            e.preventDefault();
+        });
+    });
+});
+
+function Ajaxcart(url) {
+    this.url = url;
+    var minicartOptions  = {
+        formKey:    $j('#form_key').html()
+    }
+    this.Mini = new Minicart(minicartOptions);
+};
+
+Ajaxcart.prototype = {
+    addToCart: function(data) {
+        // Append Minicart formKey to the data so form actions function
+        data['formKey'] = this.Mini.formKey;
+
+        // Scroll back to the top
+//        $j("html, body").animate({ scrollTop: 0 }, "slow");
+
+        // Show minicart w/ loading overlay
+        this.showMinicart(true);
+
+        $j.ajax({
+            url: this.url,
+            dataType: "json",
+            context: this,
+            data: data
+        }).done(function(result) {
+            // Replace Minicart HTML
+            $j('.header-minicart').html(result.minicart_html);
+
+            // Persist minicart visibility
+            this.showMinicart(false);
+
+            // Add success message
+            this.Mini.showSuccess(result.success);
+
+            // Rebind Minicart actions
+            $j(document).trigger('rebind-minicart');
+
+            // Reinit Minicart
+            this.Mini.init();
+        }).error(function(e) {
+            console.log('AJAX Cart Error');
+        });
+    },
+
+    // Shows minicart with loading overlay
+    showMinicart: function(loading) {
+        $j('.header-minicart a').addClass('skip-active');
+        $j('#header-cart').addClass('skip-active');
+        if (loading) {
+            this.Mini.showOverlay();
+        }
+        this.Mini.hideMessage();
+    }
+};
+
