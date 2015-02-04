239b723c0fa6ae255f10b705eb7a9ca7bee348d8
Sam Tay <sam.tay@blueacorn.com>
2015-02-04 15:49:52 -0500

    override wishlist controller - include dependencies in app/etc xml

---
+++ app/code/local/BlueAcorn/AjaxCart/controllers/Wishlist/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package     BlueAcorn\AjaxCart
+ * @version     
+ * @author      Blue Acorn, Inc. <code@blueacorn.com>
+ * @copyright   Copyright © 2015 Blue Acorn, Inc.
+ */
+class BlueAcorn_AjaxCart_Wishlist_IndexController extends Mage_Wishlist_IndexController
+{
+    
+}
