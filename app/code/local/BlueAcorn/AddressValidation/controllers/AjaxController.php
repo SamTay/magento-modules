023c00af27a33e51bb1e40f875287840af166e89
Sam Tay <sam.chong.tay@gmail.com>
2015-06-26 16:41:02 -0400

    add controllers and layout update for js file

---
+++ app/code/local/BlueAcorn/AddressValidation/controllers/AjaxController.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package     BlueAcorn\AddressValidation
+ * @version     0.1.0
+ * @author      Blue Acorn, Inc. <code@blueacorn.com>
+ * @copyright   Copyright © 2015 Blue Acorn, Inc.
+ */
+class BlueAcorn_AddressValidation_AjaxController extends Mage_Core_Controller_Front_Action
+{
+}
---
+++ app/code/local/BlueAcorn/AddressValidation/controllers/Checkout/OnepageController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package     BlueAcorn\AddressValidation
+ * @version     0.1.0
+ * @author      Blue Acorn, Inc. <code@blueacorn.com>
+ * @copyright   Copyright © 2015 Blue Acorn, Inc.
+ */
+require_once Mage::getModuleDir('controllers', 'Mage_Checkout') . DS . 'OnepageController.php';
+
+class BlueAcorn_AddressValidation_Checkout_OnepageController
+    extends Mage_Checkout_OnepageController
+{
+
+}
