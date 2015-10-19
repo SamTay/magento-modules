ac67c300eaa858170cbd38a4b65f2c9d0b7b0ff9
Sam Tay <sam.chong.tay@gmail.com>
2015-10-19 15:10:39 -0400

    Integration for usps city/state lookup

---
+++ app/code/local/BlueAcorn/AddressValidation/Model/Zipcode/ApiInterface.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package     BlueAcorn\AddressValidation
+ * @version     0.1.0
+ * @author      Sam Tay @ Blue Acorn, Inc. <code@blueacorn.com>
+ * @copyright   Copyright © 2015 Blue Acorn, Inc.
+ */
+
+/**
+ * Interface BlueAcorn_AddressValidation_Model_Zipcode_ApiInterface
+ * All zipcode lookup tools are required to implement this interface
+ */
+interface BlueAcorn_AddressValidation_Model_Zipcode_ApiInterface
+{
+    /**
+     * @param string $zipcode
+     * @return array
+     */
+    public function lookupZipcode($zipcode);
+}
