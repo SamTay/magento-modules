a61d30c80707a11ea9a9fb6b4f0f6300d1eb2b2c
Sam Tay <sam.chong.tay@gmail.com>
2015-10-21 22:52:29 -0400

    Remove unused source model methods

--- app/code/local/BlueAcorn/AddressValidation/Model/System/Config/Source/Api/Domestic.php
+++ app/code/local/BlueAcorn/AddressValidation/Model/System/Config/Source/Api/Domestic.php
@@ -28,28 +28,4 @@ class BlueAcorn_AddressValidation_Model_System_Config_Source_Api_Domestic
             )
         );
     }
-
-    /**
-     * Get the text labels for given values
-     *
-     * @param $values
-     * @param bool $string
-     * @return array|string
-     */
-    public function getOptionText($values, $string = false)
-    {
-        $options = $this->toOptionArray();
-        if (!is_array($values)) {
-            $values = explode(',', $values);
-        }
-
-        $texts = array();
-        foreach($values as $value) {
-            if (isset($options[$value])) {
-                $texts[] = $options[$value];
-            }
-        }
-
-        return ($string) ? implode(',', $texts) : $texts;
-    }
 }
