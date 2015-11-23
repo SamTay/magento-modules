85fe177b7a22bcb26e545fe8965b4a2be77ecb53
Sam Tay <sam.chong.tay@gmail.com>
2015-11-23 13:39:27 -0500

    Boilerplate magicento module + modman file

---
+++ app/code/local/BlueAcorn/AttributeFlag/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package     BlueAcorn\AttributeFlag
+ * @version     0.1.0
+ * @author      Sam Tay @ Blue Acorn, Inc. <code@blueacorn.com>
+ * @copyright   Copyright © 2015 Blue Acorn, Inc.
+ */ 
+class BlueAcorn_AttributeFlag_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
---
+++ app/code/local/BlueAcorn/AttributeFlag/sql/ba_attributeflag_setup/install-0.1.0.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package     BlueAcorn\AttributeFlag
+ * @version     0.1.0
+ * @author      Sam Tay @ Blue Acorn, Inc. <code@blueacorn.com>
+ * @copyright   Copyright © 2015 Blue Acorn, Inc.
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
