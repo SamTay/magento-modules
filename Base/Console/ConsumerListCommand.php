4488c84289e4179f2e37d2db8dc6a41099cc575c
Sam Tay <sam.chong.tay@gmail.com>
2016-05-11 10:31:26 -0400

    Rewrite list command to include count, closes #9

---
+++ Base/Console/ConsumerListCommand.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @package     BlueAcorn\AmqpBase
+ * @version     1.0.0
+ * @author      Sam Tay @ Blue Acorn, Inc. <code@blueacorn.com>
+ * @copyright   Copyright © 2016 Blue Acorn, Inc.
+ */
+namespace BlueAcorn\AmqpBase\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use BlueAcorn\AmqpBase\Helper\MessageQueue\Config as QueueConfig;
+use BlueAcorn\AmqpBase\Model\Consumer\Daemonizer;
+
+/**
+ * Command for starting MessageQueue consumers.
+ */
+class ConsumerListCommand extends Command
+{
+    const COMMAND_QUEUE_CONSUMERS_LIST = 'queue:consumers:list';
+
+    /**
+     * @var QueueConfig
+     */
+    protected $queueConfig;
+
+    /**
+     * @var Daemonizer
+     */
+    protected $daemonizer;
+
+    /**
+     * @param QueueConfig $queueConfig
+     * @param Daemonizer $daemonizer
+     * @param string|null $name
+     */
+    public function __construct(
+        QueueConfig $queueConfig,
+        Daemonizer $daemonizer,
+        $name = null
+    ) {
+        $this->queueConfig = $queueConfig;
+        $this->daemonizer = $daemonizer;
+        parent::__construct($name);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        foreach($this->queueConfig->getConsumersList() as $consumerName) {
+            $output->writeln(sprintf('%30s: %d', $consumerName, $this->daemonizer->getCurrentDaemonCount($consumerName)));
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName(self::COMMAND_QUEUE_CONSUMERS_LIST);
+        $this->setDescription('List of consumers');
+        $this->setHelp(
+            <<<HELP
+This command shows list of consumers and the current number of daemon processes.
+HELP
+        );
+        parent::configure();
+    }
+}
