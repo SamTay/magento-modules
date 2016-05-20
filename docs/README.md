5067d5428594a0b941c6db143b4aced3b008fbee
Sam Tay <sam.chong.tay@gmail.com>
2016-05-20 01:41:08 -0400

    Add example module for documentation

---
+++ docs/README.md
@@ -0,0 +1,29 @@
+# Documentation
+Refer to specific submodule documentation for details on the amqp-integrations framework that this congolomerate provides.
+This `docs` directory shows how these modules can be used in a particular site implementation. See the `docs/examples` subdirectory
+for example modules that will live in client repositories.
+
+## Examples
+#### Product Integration
+See this module for an example of leveraging this framework for specific PIM imports. Note the `entity_map_import.xml` file,
+which automatically handles three important sequential steps:
+
+###### 1. Simple key maps
+  - xml: `<key_map from="externalKey" to="internal_key" />`
+  - result: `['externalKey' => $value] -> ['internal_key' => $value]`
+  
+###### 2. Aggregating attributes
+  - xml: `<aggregate id="aggregate"><key id="first_piece" /><key id="second_piece" /></aggregate>`
+  - result: `['first_piece' => '1', 'second_piece => '2'] -> ['aggregate' => ['first_piece' => '1', 'second_piece => '2']]`
+  - note: This will mainly be useful for attribute mapping after aggregation
+  
+###### 3. Attribute key,value maps
+  - xml: `<attribute_map code="color" mapper="BlueAcorn\EntityMap\Mapper\OptionTable />`
+  - result: `['color' => 'blue,green'] -> ['color' => '82,24']`
+  
+###### 4. Default mapper specification
+  - xml: `<default_mapper class="BlueAcorn\EntityMap\Mapper\OneToOne" />`
+  - result: maps anything not specified via this mapper class
+
+## Support
+If the documentation here is inadequate, please submit a github issue.
---
+++ docs/examples/ProductIntegration/Model/Mapper/Attribute/ConfidenceBuilder.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @package     BlueAcorn\ProductIntegration
+ * @version     1.0.0
+ * @author      Sam Tay @ Blue Acorn, Inc. <code@blueacorn.com>
+ * @copyright   Copyright © 2016 Blue Acorn, Inc.
+ */
+namespace BlueAcorn\ProductIntegration\Model\Mapper\Attribute;
+
+use BlueAcorn\EntityMap\MapperInterface;
+
+class ConfidenceBuilder implements MapperInterface
+{
+    const ATTRIBUTE_CODE = 'confidence_builder';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function map($key, $value)
+    {
+        $this->validateKey($key);
+        $values = [];
+        foreach($value as $option => $flag) {
+            if ($flag) {
+                $values[] = $this->getOptionId($option);
+            }
+        }
+
+        return [$key, implode(',', $values)];
+    }
+
+    /**
+     * Get option id from string code
+     *
+     * @param $optionString
+     * @return int
+     */
+    protected function getOptionId($optionString)
+    {
+        // Dummy method
+        // Pretend this maps 'money_back_guarantee' to a custom source model value such as (int)2
+        return (int)$optionString;
+    }
+
+    /**
+     * Validate attribute code
+     *
+     * @param $key
+     * @throws \LogicException
+     */
+    private function validateKey($key)
+    {
+        if ($key !== self::ATTRIBUTE_CODE) {
+            throw new \LogicException(__(
+                "%class should not be used for attributes other than %code",
+                ['class' => get_class($this), 'code' => self::ATTRIBUTE_CODE]
+            ));
+        }
+    }
+}
