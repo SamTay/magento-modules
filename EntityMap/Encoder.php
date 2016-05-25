a3a5f33221f0af50ad231aff956d87544dfa38ff
Sam Tay <sam.chong.tay@gmail.com>
2016-05-25 14:23:11 -0400

    Add empty Encoder for now, so that Converter can run

---
+++ EntityMap/Encoder.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package     BlueAcorn\EntityMap
+ * @version     1.0.0
+ * @author      Sam Tay @ Blue Acorn, Inc. <code@blueacorn.com>
+ * @copyright   Copyright © 2016 Blue Acorn, Inc.
+ */
+namespace BlueAcorn\EntityMap;
+
+class Encoder
+{
+}
