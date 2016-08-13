b3df8b965ee2b9bca5a9095a4b6142679161095f
Sam Tay <sam.chong.tay@gmail.com>
2016-08-12 20:25:28 -0400

    Use resource model to handle updating facets

--- Model/FacetPool.php
+++ Model/FacetPool.php
@@ -7,15 +7,19 @@
  */
 namespace BlueAcorn\LayeredNavigation\Model;
 
+use Magento\Catalog\Model\Category;
 use Magento\Catalog\Model\Layer;
-use Magento\Catalog\Model\Layer\Filter\FilterInterface;
+use BlueAcorn\LayeredNavigation\Model\ResourceModel\Facet as FacetResource;
 use Magento\Catalog\Model\Layer\Resolver as LayerResolver;
 use Magento\Catalog\Model\ResourceModel\Eav\Attribute;
 use Magento\Catalog\Model\ResourceModel\Product\Collection as ProductCollection;
 use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory as ProductCollectionFactory;
+use Magento\Framework\Phrase;
 
 /**
  * Class FacetPool
+ * @method addAttributeFilter(Attribute $attribute, string $attributeValue)
+ * @method addCategoryFilter(Category $category)
  */
 class FacetPool
 {
@@ -28,45 +32,57 @@ class FacetPool
     /** @var ProductCollectionFactory */
     protected $collectionFactory;
 
+    /** @var FacetResource */
+    protected $resource;
+
     /** @var Facet[] */
     protected $facets = [];
 
     /** @var array */
-    protected $methodStack;
+    protected $methodStack = [];
+
+    /** @var array */
+    private $allowedMethods = ['addAttributeFilter', 'addCategoryFilter'];
+
 
     /**
      * CollectionPool constructor.
      * @param LayerResolver $layerResolver
      * @param ProductCollectionFactory $collectionFactory
      * @param FacetBuilder $facetBuilder
+     * @param FacetResource $resource
      */
     public function __construct(
         LayerResolver $layerResolver,
         ProductCollectionFactory $collectionFactory,
-        FacetBuilder $facetBuilder
+        FacetBuilder $facetBuilder,
+        FacetResource $resource
     ) {
         $this->layer = $layerResolver->get();
         $this->collectionFactory = $collectionFactory;
         $this->facetBuilder = $facetBuilder;
+        $this->resource = $resource;
     }
 
     /**
      * Add facet to pool based on current layer collection
-     * TODO This might not be the right approach... excluding the attribute A=>1 filter and counting
-     * results for attribute A=>2 is not the same as counting attribute A=>1 OR A=>2. Shiet
      *
      * @param Attribute $attribute
+     * @param $attributeValue
      */
-    public function addFacet(Attribute $attribute)
+    public function addFacet(Attribute $attribute, $attributeValue)
     {
         // TODO what should happen if array key already exists??
         // TODO possibly filter against existence in FilterableAttributesList
         // TODO maybe check if $field is a non price/decimal attribute ?? Not sure ...
         // if all price/decimals are using sliders then we shouldn't worry about faceting against them
-        $this->facets[$attribute->getAttributeCode()] = $this->facetBuilder
+        $facet = $this->facetBuilder
             ->setCollection($this->mirrorCollection())
             ->setAttribute($attribute)
+            ->setAttributeValue($attributeValue)
             ->create();
+        $this->addAttributeFilter($attribute, $attributeValue);
+        $this->facets[$attribute->getAttributeCode()] = $facet; // Add to facet pool after attribute filter is applied
     }
 
     /**
@@ -81,20 +97,25 @@ class FacetPool
     }
 
     /**
-     * Add collection modifier to methodStack and apply to all current facets
-     *
-     * @param $method
-     * @param $args
+     * Update all current facets with the latest addition to methodStack
      */
-    public function addCollectionModifier($method, $args = [])
+    private function updateFacets()
     {
-        // modify current facets
-        foreach($this->facets as $facet) {
-            if (!$facet->shouldSkip($method, $args)) {
-                call_user_func_array([$facet->getCollection(), $method], $args);
-            }
+        $modifier = end($this->methodStack);
+        if (!$modifier) {
+            return;
         }
-        $this->addToModifierStack($method, $args);
+        $method = $modifier['method'];
+        $args = $modifier['args'];
+        foreach($this->facets as $attributeCode => $facet) {
+            if ($this->shouldSkip($attributeCode, $method, $args)) {
+                continue;
+            }
+            call_user_func_array(
+                [$this->resource, $method],
+                array_merge([$facet->getCollection()], $args)
+            );
+        }
     }
 
     /**
@@ -102,45 +123,59 @@ class FacetPool
      *
      * @return ProductCollection
      */
-    protected function mirrorCollection()
+    private function mirrorCollection()
     {
         $collection = $this->collectionFactory->create();
-        foreach($this->methodStack as $func) {
-            call_user_func_array([$collection, $func['method']], $func['args']);
+        foreach($this->methodStack as $modifier) {
+            call_user_func_array(
+                [$this->resource, $modifier['method']],
+                array_merge([$collection], $modifier['args'])
+            );
         }
         return $collection;
     }
 
     /**
-     * Add modifier to method stack
+     * Magic call implemented for forwarding specific methods to facet resource
      *
      * @param $method
-     * @param $args
+     * @param $arguments
      */
-    protected function addToModifierStack($method, $args)
+    public function __call($method, $arguments)
     {
-        if (!$this->isBlacklisted($method, $args)) {
-            $this->methodStack[] = ['method' => $method, 'args' => $args];
+        if (!in_array($method, $this->allowedMethods)) {
+            throw new \BadMethodCallException(
+                new Phrase('Invalid method %1::%2(%3)', [get_class($this), $method, print_r($arguments, 1)])
+            );
         }
+        // Update method stack for future mirrors
+        $this->methodStack[] = [
+            'method' => $method,
+            'args' => $arguments
+        ];
+        // Update current facets
+        $this->updateFacets();
     }
 
     /**
-     * Check if modifier is blacklisted
-     * TODO refactor this loop through array property of callables
+     * Check if filter would interfere with attribute facet..
+     * This looks a bit dirty but is just a fail safe. We are adding facet to pool after applying
+     * attribute filter, so this could be removed if wanted.
      *
+     * @param $attributeCode
      * @param $method
      * @param $args
      * @return bool
      */
-    protected function isBlacklisted($method, $args)
+    private function shouldSkip($attributeCode, $method, $args)
     {
-        switch(true) {
-            case ($method == 'addFieldToFilter' && $args[0] == 'category_ids'):
-                return true;
-            case ($method == 'addFieldToFilter' && $args[0] == 'visibility'):
-                return true;
-            default:
-                return false;
+        if ($method == 'addAttributeFilter'
+            && isset($args[0])
+            && $args[0] instanceof Attribute
+            && $args[0]->getAttributeCode() == $attributeCode
+        ) {
+            return true;
         }
+        return false;
     }
 }
